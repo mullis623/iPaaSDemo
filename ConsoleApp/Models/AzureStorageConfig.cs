b591d41f5b89119f33f08d19d8d4bc996df8a998
Shaun Mullis <shmulli@microsoft.com>
2020-10-23 18:37:54 -0400

    MVP Completion - Use Console App to upload image with metadata

--- ConsoleApp/Models/AzureStorageConfig.cs
+++ ConsoleApp/Models/AzureStorageConfig.cs
@@ -9,6 +9,6 @@ namespace ConsoleApp.Models
         public string AccountName { get; set; }
         public string AccountKey { get; set; }
         public string ImageContainer { get; set; }
-        public string ThumbnailContainer { get; set; }
+        public string MetaContainer { get; set; }
     }
 }
